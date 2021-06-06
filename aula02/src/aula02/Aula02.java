b9b50c1fbe1b2f614eccee939fa5fe48daf62630
Marcus Gularte <marcus.gbatista@gmail.com>
2021-06-06 01:34:38 -0300

    teste

--- aula02/src/aula02/Aula02.java
+++ aula02/src/aula02/Aula02.java
@@ -4,6 +4,7 @@ public class Aula02 {
 
     public static void main(String[] args) {
         System.out.println("olá java");
+        System.out.println("Essa linha eu adicionei depois ");
     }
     
 }
